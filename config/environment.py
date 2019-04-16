8a0cec9efb41853e0a327af961850c0cb8e7e303
Matthew Whaley <mjwhaley@mac.com>
2019-04-16 14:39:10 +0100

    Updated for Deployment

--- config/environment.py
+++ config/environment.py
@@ -1,3 +1,4 @@
 import os
 
+db_uri = os.getenv('DATABASE-URL', 'postgres://localhost:5432/entries')
 secret = os.getenv('SECRET', 'ssh its a secret')
--- config/routes.py
+++ config/routes.py
@@ -1,7 +1,16 @@
+import os
 from app import app
-
 from controllers import entries, categories, auth
 
 app.register_blueprint(entries.api, url_prefix='/api')
 app.register_blueprint(categories.api, url_prefix='/api')
 app.register_blueprint(auth.api, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
