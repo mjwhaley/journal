8ffbc6837d2cd169f77a4437badc3b835e8efd66
Matthew Whaley <mjwhaley@mac.com>
2019-04-16 14:46:31 +0100

    Updated for Deployment

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE-URL', 'postgres://localhost:5432/entries')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/entries')
 secret = os.getenv('SECRET', 'ssh its a secret')
